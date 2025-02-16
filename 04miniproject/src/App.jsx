04ea1e6967be7ffb79acc772dc4a7852def011ca
Poojanagude <poojanagude@gmail.com>
2025-02-16 16:20:30 +0530

    themeChange

--- 04miniproject/src/App.jsx
+++ 04miniproject/src/App.jsx
@@ -3,13 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import UserContextProvider from './context/UserContextProvider';
+import Profile from './components/Profile';
+import Login from './components/Login';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <UserContextProvider>
-     <h1>Hello World</h1>
+     <Login/>
+     <Profile/>
     </UserContextProvider>
   )
 }
--- 04miniproject/src/components/Login.jsx
+++ 04miniproject/src/components/Login.jsx
@@ -17,6 +17,7 @@ const Login = () => {
         value={username}
         onChange={(e)=>setUsername(e.target.value)}
          placeholder='username'/>
+         {"    "} 
         <input type='text'
         value={password}
         onChange={(e)=>setPassword(e.target.value)}
